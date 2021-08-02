c3ae9255051442f41dc3069d84d6dfdd8d1e2693
Holms <62711445+Ho1ms@users.noreply.github.com>
2021-08-02 22:28:36 +0300

    finaly

--- text.js
+++ text.js
@@ -6,12 +6,8 @@ let check = false
 
 setInterval(() => {
     if (len >= header.length) return
-
-    text.innerText += header[len];
+    text.innerHTML += header[len];
     len++;
 }, 140)
 
-setInterval(() => {
-    check = !check
-    text.innerText = check ? header.substr(0, len-1) : header.substr(0, len)
-}, 1000)
+
